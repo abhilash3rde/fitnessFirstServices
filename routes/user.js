0b6384b10e4fe8028abc6650865816f58a995690
yatan.vesh <yatan.vesh@gmail.com>
2020-07-10 09:03:49 +0530

    Gets subscriptions for both user and trainer

--- routes/user.js
+++ routes/user.js
@@ -6,19 +6,19 @@ const {saveFileToServer} = require('../config/uploadConfig');
 const TrainerData = require('../models/trainerData');
 const UserData = require('../models/userData');
 const User = require('../models/user');
-const {userTypes} =  require("../constants");
+const {userTypes} = require("../constants");
 const Subscription = require('../models/Subscription');
 
 router.get('/myInfo', async function (req, res, next) {
   try {
-    
+
     const {userId} = req;
 
     let user = await TrainerData.getById(userId);
 
-    if(!user){
+    if (!user) {
       user = await UserData.getById(userId);
-    } 
+    }
 
     if (!user) throw new Error('Internal server error. code 45621');
 
@@ -33,7 +33,7 @@ router.put('/', async function (req, res, next) {
   try {
     console.log(`User ${req.userId} update request`);
     const {userId, userType} = req;
-    let model = userType === userTypes.TRAINER? TrainerData: UserData;
+    let model = userType === userTypes.TRAINER ? TrainerData : UserData;
     const userData = await model.edit(
       userId,
       {
@@ -59,7 +59,7 @@ router.put('/displayImage', async function (req, res, next) {
     const contentURL = content.contentURL;
 
     const {userId, userType} = req;
-    let model = userType === userTypes.TRAINER? TrainerData: UserData;
+    let model = userType === userTypes.TRAINER ? TrainerData : UserData;
 
     const userData = await model.edit(
       userId,
@@ -89,7 +89,7 @@ router.put('/wallImage', async function (req, res, next) {
     const contentURL = content.contentURL;
 
     const {userId, userType} = req;
-    let model = userType === userTypes.TRAINER? TrainerData: UserData;
+    let model = userType === userTypes.TRAINER ? TrainerData : UserData;
 
     const userData = await model.edit(
       userId,
@@ -109,13 +109,15 @@ router.put('/wallImage', async function (req, res, next) {
 
 router.get('/mySubscriptions', async function (req, res, next) {
   try {
-    const { userId } = req;
-    const subscriptions = await Subscription.getAllForUser(userId);
-    
-    if(!subscriptions) {
+    const {userId, userType} = req;
+    const subscriptions = userType === userTypes.USER ?
+      await Subscription.getAllForUser(userId) :
+      await Subscription.getAllForTrainer(userId);
+
+    if (!subscriptions) {
       throw Error("No Subscriptions")
     }
-    res.json( subscriptions );
+    res.json(subscriptions);
   } catch (err) {
     res.status(500).json({
       err: err.message

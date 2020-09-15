27ebbf3bb265066b1dc1d08781340f8230a5c83f
Devesh Gupta <70230227+Dgdevesh@users.noreply.github.com>
2020-09-15 20:05:11 +0530

    initial comit

--- config/db.js
+++ config/db.js
@@ -1,9 +1,39 @@
 const mongoose = require('mongoose');
+ const mode = ['dev','prod','testing']
+ const modee=process.env.mode || mode[1]
+ console.log(modee,'1')
+let dbName
+let dbString
 
-const dbName = process.env.dbName || 'delta';
-mongoose.connect(
-  process.env.MONGO_URI || `mongodb+srv://boi:244466666@cluster0-nssjy.mongodb.net/${dbName}?retryWrites=true&w=majority`,
+ switch (mode[0]){
+   case 'dev':
+     console.log('dev')
+     dbName = 'Cluster0';
+ dbString=`mongodb+srv://GymAddaDev:GymAddaDev@cluster0.xptfc.mongodb.net/${dbName}>?retryWrites=true&w=majority` //dev cluster
+ mongoose.connect(
+  process.env.MONGO_URI || dbString,
   { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology:true }
 )
-
-module.exports = mongoose;
+break;
+case "testing":
+  console.log('testing')
+  dbName =  'delta';
+ dbString=`mongodb+srv://boi:244466666@cluster0-nssjy.mongodb.net/${dbName}?retryWrites=true&w=majority` //Yatan Cluster
+mongoose.connect(
+  process.env.MONGO_URI || dbString,
+  { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology:true }
+)
+break;
+case 'prod':
+     console.log('production')
+     dbName ='GymAdda';
+ dbString=`mongodb+srv://qqqqqqqq9:qqqqqqqq9@gymadda.auw7i.mongodb.net/${dbName}?retryWrites=true&w=majority` //devesh Cluster
+ mongoose.connect(
+  process.env.MONGO_URI || dbString,
+  { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology:true }
+)
+break;
+ }
+ 
+ module.exports = mongoose;
+ 

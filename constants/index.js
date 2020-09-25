353960882eae028eecb697fb59c2a570fa61f040
Devesh Gupta <70230227+Dgdevesh@users.noreply.github.com>
2020-09-25 20:58:58 +0530

    changes in constants

--- constants/index.js
+++ constants/index.js
@@ -60,9 +60,9 @@ const TIME_STRING = ['0000', '0100', '0200', '0300', '0400',
   '2000', '2100', '2200', '2300'];
 
 const agoraApps = {
-  primary: '2a11008a3f634ee99d4bf91a8197a9fb', //keepmefit34
-  secondary: '2a11008a3f634ee99d4bf91a8197a9fb', //keepmefit34
-  // secondary: 'da0c4d0c5bcb47799554eb975cb8f86a' //roopesh.namdev12
+  // primary: '2a11008a3f634ee99d4bf91a8197a9fb', //keepmefit34
+  primary: 'da0c4d0c5bcb47799554eb975cb8f86a', //roopesh.namdev12
+  secondary: 'da0c4d0c5bcb47799554eb975cb8f86a' //roopesh.namdev12
 }
 const agoraAppIds = [agoraApps.primary, agoraApps.secondary]
 //test key
